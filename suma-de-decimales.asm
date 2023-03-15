c82ffc8ddddcac2533bc8f6ce68ddeb046aedb06
andyfer <andyfer282810@gmail.como>
2023-03-15 14:41:38 -0400

    primer programa

---
+++ suma-de-decimales.asm
@@ -0,0 +1,37 @@
+MOV AX, 1
+MOV CX, 19    
+MOV NUM1, CX ;ALMACENAMOS EL NUESTRO DIVISOR
+
+MOV CX, 15
+INC CX
+ciclo:
+    CMP AX, NUM1 ; COMPROBAMOS QUE SE PUEDA DIVIDIR
+    JC ESMENOR   ; POR NO AUMENTAMOS UN 0
+    
+    MOV DX, 0     ; ANTES DE DIVIDIR DX=0
+    DIV NUM1
+    MOV BX, CONT
+    ADD CONT, AX
+    MOV BX, CONT
+    MOV AX, DX
+    MOV BX, 10 
+    PUSH DX
+    MUL BX
+    POP DX
+    CMP DX, 0
+    JZ FIN         
+    loop ciclo
+JMP FIN
+
+ESMENOR:
+    MOV BX, 10
+    MUL BX
+    DEC CX
+    JMP ciclo ;salto a ciclo
+FIN:
+    MOV BX, CONT
+ret
+
+;VARIABLES 
+NUM1 DW 0
+CONT DW 0
