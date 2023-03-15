efb508344ce678c7deaec247d1421463f165a2a9
andyfer <andyfer282810@gmail.como>
2023-03-15 18:38:12 -0400

    Algunos arreglos minimos

--- suma-de-decimales.asm
+++ suma-de-decimales.asm
@@ -3,12 +3,27 @@
 ; Ejemplo
 ; 1/19 = 0.052631578947368
 ; suma = 74 en BX = 4A 
+data segment
+    ; add your data here!
+    suma db "LA SUMA ES: $"
+ends
+
+stack segment
+    dw   128  dup(0)
+ends
+
+code segment
+start:
+; set segment registers:
+    mov ax, data
+    mov ds, ax
+    mov es, ax
 
 MOV AX, 1
 MOV CX, 19    
 MOV NUM1, CX ;ALMACENAMOS EL NUESTRO DIVISOR
 
-MOV CX, 4
+MOV CX, 15
 INC CX
 ciclo:
     CMP AX, NUM1 ; COMPROBAMOS QUE SE PUEDA DIVIDIR
@@ -34,28 +49,22 @@ ESMENOR:
     MUL BX
     DEC CX
     JMP ciclo ;salto a ciclo
+
 FIN:
     MOV BX, CONT
     
-    MOV DL, 2
+    
+    LEA DX, SUMA
+    MOV AH, 9
+    INT 21H 
+          
+    MOV DX, BX
     ADD DL, 48
     MOV AH, 2
     INT 21H
-    
-    MOV DL, 4
-    ADD DL, 48
-    MOV AH, 2
-    INT 21H
-    
-    MOV DL, 32
-    MOV AH, 2
-    INT 21H
-    
-    MOV DL, 65
-    MOV AH, 2
-    INT 21H
-ret
 
 ;VARIABLES 
 NUM1 DW 0
-CONT DW 0
+CONT DW 0  
+
+end start
