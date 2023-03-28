739c3b97e818e99299738cf5b5a0c74455c9f0bc
andyfer <andyfer282810@gmail.como>
2023-03-28 07:43:36 -0400

    Comentarios específicos en el template

--- template.asm
+++ template.asm
@@ -3,6 +3,8 @@
 data segment
     ; add your data here!
     pkey db "press any key...$"
+    welcom db "Bienbenido al sistema!$"
+    
 ends
 
 stack segment
@@ -12,13 +14,20 @@ ends
 code segment
 start:
 ; set segment registers:
+    ; Carga la direccion del segmento de datos en el registro AX
     mov ax, data
-    mov ds, ax
-    mov es, ax
+
+    ; Inicializa el registro DS con la direccion del segmento de datos en el registro AX
+    ; DS significa "Data Segment" (Segmento de Datos)
+    mov ds, ax     
+
+    ; Inicializa el registro ES con la direccion del segmento de datos en el registro AX
+    ; ES significa "Extra Segment" (Segmento Extra)
+    mov es, ax     
 
     ; add your code here
             
-    lea dx, pkey   ; Load Effective Address
+    lea dx, pkey
     mov ah, 9
     int 21h        ; output string at ds:dx
     
@@ -26,8 +35,12 @@ start:
     mov ah, 1
     int 21h
     
+    lea dx, welcom
+    mov ah, 9
+    int 21h
+    
     mov ax, 4c00h ; exit to operating system.
     int 21h    
 ends
 
-end start ; set entry point an  d stop the assembler.
+end start ; set entry point and stop the assembler.
