8592c6cd65d6ea088934c330f6355b22db2432ba
andyfer <andyfer282810@gmail.como>
2023-05-13 16:06:28 -0400

    Primera parte teriminada, ahora si resuelve el problema

--- ejecicioDeVectores/elUlimoManda.asm
+++ ejecicioDeVectores/elUlimoManda.asm
@@ -4,29 +4,30 @@ data segment
     enter db 10,13,'$'
     len dw 0
     contador dw 0
-    ultimo dw 0
     contador2 dw 0
+    
+    maxCont dw 0 
+    comparador dw 0
 ends
 
-; Nue
 code segment
 start:
     mov ax, data
     mov ds, ax
-    
+       
     call pideNumero      ; Pedimos la longitud del vector
     call return  
     mov ax,numero
     mov len, ax          ; Guardamos la longitud en "len"
     
-    lea si, v            ; Asignamos la vase el vector "v"
-    
+        
+    mov si, offset v     ; lea si, v     ; Asignamos la vase el vector "v"
     mov cx, len
     loop_while0:
         cmp cx, 0        ; CX == 0?
         jz fin_while0    ; True   
                          ; False (continue)
-        mov contador, cx ; Guardamos CX
+        push cx
         call pideNumero
         call return
         
@@ -35,47 +36,61 @@ start:
         mov [si], al
         inc si
         
-        mov cx, contador
+        pop cx
         loop loop_while0 ; volver al inicio del loop
         
     fin_while0:
 
-    mov ax, numero
-    mov ultimo, ax       ; Guardamos ultimo <- numero
-    
-    ; El siguente codigo solo cuenta cuantas veces se repite
-    ;   el ultimo. 
-    lea si, v
-    mov cx, len          
-    loop_while:
-        cmp cx, 0 
-        jz fin_while 
-        mov contador, cx
-        
-        xor bx, bx
-        mov bl, [si] 
-        inc si
-        mov ax, ultimo 
-        cmp bx, ax 
-        je iguales
-        jmp continuar
-         
-        iguales:
-        add contador2, 1 
 
-        continuar:
-        mov cx, contador
-        loop loop_while     
-    fin_while:   
+    mov di, offset v     ;lea di, v 
+    mov cx, len
+    mov maxCont, 0
+     
+    while_externo:
+        cmp cx, 0
+        jz fin_while_externo
+        
+        xor ax, ax
+        mov al, [di] 
+        inc di
+        mov comparador, ax
+        push cx
+         
+        mov si, offset v  ;lea si, v
+        mov contador2, 0
+        mov cx, len         
+        loop_while:
+            cmp cx, 0 
+            jz fin_while 
+            
+            xor ax, ax
+            mov al, [si] 
+            inc si
+            
+            cmp ax, comparador
+            jne no_son_iguales
+            inc contador2 
+            no_son_iguales:
+            
+            loop loop_while     
+        fin_while:
+        mov ax, contador2
+        cmp ax, maxCont
+        jle no_es_mayor   ; Salta si ax <= maxCont
+        mov maxCont, ax
+        no_es_mayor:
+        call imprime
+        pop cx 
+        loop while_externo
+    fin_while_externo:       
     
     call return
     
-    mov ax, contador2
+    mov ax, maxCont
     call imprime  
-
-    ; Salida del programa
-    mov ax, 4c00h
+    mov ax, 4c00h         ; Fin del progrma
     int 21h
+
     
     pideNumero:
         mov numero,0
@@ -95,7 +110,6 @@ start:
             jnz leerNumero
         finLectura:
         ret    
-
     imprime:
         mov bx,10
         mov cx,0
@@ -111,13 +125,8 @@ start:
             pop dx
             add dl, 48
             mov ah,2
-            int 21h
-            ;loop mostrar  
-            ;lea dx, enter
-            ;mov ah, 9
-            ;int 21h      
+            int 21h       
             ret       
-
     return:
         lea dx, enter
         mov ah, 9
